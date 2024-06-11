5b12ea0da054d4fac33f5366e66a0ea3b45ecf65
PCV-Karthik <karthik.pcv@gmail.com>
2024-06-11 19:36:44 +0530

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div className="container-Nav">
         <div className="wrapper" style={{padding:'20px'}}>
           <div className='flex center'>
-            <img src="public/Screenshot 2024-06-11 at 4.23.59 AM.png" alt="" className="logo" />
-            <img src="public/Screenshot 2024-06-11 at 4.26.21 AM.png" alt="" className='logo'/>
+            <img src="Screenshot 2024-06-11 at 4.23.59 AM.png" alt="" className="logo" />
+            <img src="Screenshot 2024-06-11 at 4.26.21 AM.png" alt="" className='logo'/>
           </div>
             <button className="btn">Get Free Quote</button>
         </div>
